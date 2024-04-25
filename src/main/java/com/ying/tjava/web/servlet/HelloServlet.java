39cbc6059df3db382d642610ee28b22d529f9dd8
zhengxiaoying <zhengxiaoying666@gmail.com>
2024-04-25 18:27:15 +0800

    111

---
+++ src/main/java/com/ying/tjava/web/servlet/HelloServlet.java
@@ -0,0 +1,41 @@
+package com.ying.tjava.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (name == null) {
+            resp.setHeader("Content-Type", "application/json");
+            PrintWriter writer = resp.getWriter();
+            var body = """
+                    {
+                        "code": "400", 
+                        "message": "缺少name参数"
+                    }""";
+            writer.write(body);
+            writer.flush();
+            return;
+        } else {
+            resp.setHeader("Content-Type", "application/json");
+            PrintWriter writer = resp.getWriter();
+            var body = """
+                    {
+                        "code": "200", 
+                        "message": "Hello, %s"
+                    }""".formatted(name);
+            writer.write(body);
+            writer.flush();
+        }
+    }
+
+}
---
+++ src/main/java/com/ying/tjava/web/servlet/TestServlet.java
@@ -0,0 +1,30 @@
+package com.ying.tjava.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 通过@WebServlet绑定路由
+ */
+@WebServlet("/")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //  get path param
+        String name = req.getParameter("name");
+        //  302 暂时重定向 浏览器重新发一个请求给重定向地址
+        //  301 如果服务器发送301永久重定向响应，浏览器会缓存/hi到/hello这个重定向的关联，下次请求/hi的时候，浏览器就直接发送/hello请求了
+        //  resp.sendRedirect("/hello" + (name == null ? "" : "?name=" + name));
+        //  resp.sendRedirect("https://www.baidu.com/");    //  ok!
+
+        resp.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);   //  301状态码
+        resp.setHeader("Location", "/hello" + (name == null ? "" : "?name=" + name)); //  重定向链接
+    }
+}
