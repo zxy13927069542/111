24f75ca59147149189314d88363fe1144e390af0
zxy <zhengxiaoying666@gmail.com>
2024-05-10 23:41:33 +0800

    1

--- src/main/java/com/ying/tjava/spring/AppConfig.java
+++ src/main/java/com/ying/tjava/spring/AppConfig.java
@@ -12,13 +12,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * @EnableTransactionManagement: 开启声明式事务，添加该注释后，不需要在添加@EnableAspectJAutoProxy
@@ -39,7 +43,7 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @MapperScan()
 @EnableWebMvc
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     static final Log log = LogFactory.getLog(AppConfig.class);
 
     @Value("${url}")
@@ -64,9 +68,22 @@ public class AppConfig {
     }
 
 
+    //  配置消息转换器，可搭配注解@ResponseBody将对象以json格式输出
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jacksonMessageConverter());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter() {
+        return new MappingJackson2HttpMessageConverter();
+    }
+
     @Bean(name = "dataSource")
     public DataSource getDataSource() {
         HikariDataSource ds = new HikariDataSource();
+        //  tomcat 下需要指定驱动，不然会报找不到驱动
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         ds.setJdbcUrl(url);
         ds.setUsername(userName);
         ds.setPassword(password);
--- src/main/java/com/ying/tjava/web/servlet/HelloServlet.java
+++ src/main/java/com/ying/tjava/web/servlet/HelloServlet.java
@@ -1,5 +1,6 @@
 package com.ying.tjava.web.servlet;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,11 +9,26 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:mysql://localhost:3306/tjava");
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUsername("root");
+        ds.setPassword("066311");
+        ds.setAutoCommit(true);
+        ds.setMaximumPoolSize(10);
+        try {
+            Connection conn = ds.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         String name = req.getParameter("name");
         if (name == null) {
             resp.setHeader("Content-Type", "application/json");
@@ -30,7 +46,7 @@ public class HelloServlet extends HttpServlet {
             PrintWriter writer = resp.getWriter();
             var body = """
                     {
-                        "code": "200", 
+                        "code": "200",
                         "message": "Hello, %s"
                     }""".formatted(name);
             writer.write(body);
